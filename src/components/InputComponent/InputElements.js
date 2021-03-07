7ad92cf4ff0f7b1668321df9b0a1a2ebb40f8b7d
Sukhpreet Singh <66023067+sPreet247@users.noreply.github.com>
2021-03-07 14:53:02 -0500

    g

--- src/components/InputComponent/InputElements.js
+++ src/components/InputComponent/InputElements.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-const handleSize = (size) => {
-	switch (size) {
-		case 'sm':
-			return '40px 200px';
-		case 'fw':
-			return '56px 1031px';
-		case 'ml':
-			return '150px 200px';
-		default:
-			return '56px 200px';
-	}
-};
+// const handleSize = (size) => {
+// 	switch (size) {
+// 		case 'sm':
+// 			return '40px 200px';
+// 		case 'fw':
+// 			return '56px 1031px';
+// 		case 'ml':
+// 			return '150px 200px';
+// 		default:
+// 			return '56px 200px';
+// 	}
+// };
 
 export const Heading = styled.h1`
 	position: absolute;
@@ -29,11 +29,10 @@ export const Heading = styled.h1`
 
 export const Inputs = styled.input.attrs((props) => ({
 	type: 'text',
-	size: props.size || '1em',
 }))`
-	/* padding: ${({ size }) => handleSize(size)}; */
-	margin: ${(props) => props.size};
-  padding: ${(props) => props.size};
+	width: 200px;
+	height: 50px;
+
 	box-sizing: border-box;
 	border: 1px solid #828282;
 	border-radius: 8px;
